5c73f0138acc5070ab0d0cc09208c5514439dd84
Rafael Luis Yanase de Rezende <rafael.yanase@gmail.com>
2025-12-09 23:56:55 +0000

    Update: add thread data structure.

--- philo/libs/philo.h
+++ philo/libs/philo.h
@@ -8,6 +8,12 @@
 #include <stdio.h>
 #include <stddef.h>
 
+typedef struct s_thread_data
+{
+	char			*str;
+	pthread_mutex_t	*lock;
+}	t_thread_data;
+
 void	opt_main(char **argv);
 void	standard_main(char **argv);
 ssize_t	ft_putstr_fd(int fd, char *buff);
