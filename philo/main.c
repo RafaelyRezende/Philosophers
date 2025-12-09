9f4d82a632e7d90288134b9f591ef2c478abc802
Rafael Luis Yanase de Rezende <rafael.yanase@gmail.com>
2025-12-09 23:57:41 +0000

    TODO: update routine to cast into new thread data structure.

--- philo/main.c
+++ philo/main.c
@@ -1,5 +1,28 @@
 #include "philo.h"
 
+void	*routine(void *ptr)
+{
+	ft_putstr_fd(1, buf);
+	usleep(3000000);
+	ft_putstr_fd(1, "End routine.\n");
+	return (NULL);
+}
+
+int	main(void)
+{
+	pthread_t	t1;
+	pthread_t	t2;
+	void		**start_routine;
+
+	start_routine = &routine("Start routine.\n");
+	pthread_create(&t1, NULL, *start_routine, NULL);
+	pthread_create(&t2, NULL, *start_routine, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	return (0);
+}
+
+/*
 int	main(int argc, char **argv)
 {
 	if (argc < 5 || argc > 6)
@@ -10,3 +33,4 @@ int	main(int argc, char **argv)
 		opt_main(argv);
 	return (0);
 }
+*/
