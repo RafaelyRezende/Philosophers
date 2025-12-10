2cd1b6a694f7df7a942df8fdb54c7fe9ae6b4061
Rafael Rezende <rafael.yanase@gmail.com>
2025-12-10 16:52:40 +0000

    Update: added atoi for parser

---
+++ philo/utils/ft_atoi.c
@@ -0,0 +1,26 @@
+#include "philo.h"
+
+int	ft_atoi(const char *nptr)
+{
+	long	res;
+	int		sign;
+
+	res = 0;
+	sign = 1;
+	if (!nptr)
+		return (0);
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		res += res * 10 + (*nptr - '0');
+		nptr++;
+	}
+	if (res * sign < INT_MIN || res * sign > INT_MAX)
+		return (0);
+	return ((int) res * sign);
+}
