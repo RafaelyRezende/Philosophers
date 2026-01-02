d62f865be938e743cd7fa167496d882e88bf7ae3
Rafael Rezende <rafael.yanase@gmail.com>
2026-01-02 12:06:28 +0000

    Update: add core utility functions

--- philo_bonus/include/philo_bonus.h
+++ philo_bonus/include/philo_bonus.h
@@ -23,21 +23,27 @@ typedef struct s_philo
 	int			id;
 	int			meals_eaten;
 	long long	last_meal;
+	t_table		*table;
 	pid_t		pid;
 }	t_philo;
 
 typedef struct s_table
 {
-	long	philo_count;
-	long	time_to_die;
-	long	time_to_eat;
-	long	time_to_sleep;
-	long	must_eat_count;
-	sem_t	*semaphores; // Array of semahpores
-	sem_t	*sem_write; // Equivalent to write_lock
-	sem_t	*sem_stop; // Equivalent to sim_lock
-	t_philo	*philos;
-	pid_t	*ppids;
+	long		philo_count;
+	long		time_to_die;
+	long		time_to_eat;
+	long		time_to_sleep;
+	long		must_eat_count;
+	long long	start_time;
+	sem_t		*sem_forks; // Single semahpore for the forks (pool of forks)
+	sem_t		*sem_write; // Equivalent to write_lock
+	sem_t		*sem_stop; // Equivalent to sim_lock
+	t_philo		*philos;
+	pid_t		*pids;
 }	t_table;
 
+int			parse_philos(int argc, char **argv, t_table *table);
+int			initialize_table(t_table *table);
+long long	get_time_ms(void);
+void		ft_usleep(long time_in_ms, t_table *table);
 #endif
